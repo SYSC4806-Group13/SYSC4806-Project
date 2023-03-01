5692ee4d071e145eb40f826578e02567f108742a
Millan <real.millan.wang@gmail.com>
2023-03-01 17:50:05 -0500

    Initialize backend Spring env

---
+++ Backend/src/main/java/com/SYSC4806_Group13/SYSC4806_Project/Sysc4806ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SYSC4806_Group13.SYSC4806_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sysc4806ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sysc4806ProjectApplication.class, args);
+	}
+
+}
