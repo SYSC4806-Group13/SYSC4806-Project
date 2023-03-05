2e6b632aca0988e1ccb2ceeaa6b44b993fbb6b27
GolanHassin <golhassin@gmail.com>
2023-03-04 22:40:58 -0500

    Added seller listing forms

---
+++ frontend/src/components/Seller/SellerListingForm.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { DialogActions, Button } from '@mui/material'
+import CustomTextField from 'src/components/Form/TextField';
+
+export interface ISellerListingFormProps {
+    handleCloseDialog: () => void
+}
+
+interface IFormInput {
+    isbn: string,
+    title: string,
+    author: string,
+    publisher: string,
+    description: string,
+    inventory: number,
+    price: number,
+    releaseDate: string
+}
+
+export default function SellerListingForm(props: ISellerListingFormProps) {
+    const defaultValues = {
+        isbn: "",
+        title: "",
+        author: "",
+        publisher: "",
+        description: "",
+        inventory: 0,
+        price: 0.0,
+        releaseDate: new Date().toISOString().slice(0, 10)
+    };
+    const formMethods = useForm({ defaultValues });
+    const { handleSubmit, reset, control, setValue, formState: { errors } } = formMethods;
+    const onSubmit: SubmitHandler<IFormInput> = data => {
+        console.info(data)
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <CustomTextField label='ISBN' name='isbn' number control={control} required errors={errors}/>
+            <CustomTextField label='Title' name='title' control={control} required errors={errors}/>
+            <CustomTextField label='Author' name='author' control={control} required errors={errors}/>
+            <CustomTextField label='Publisher' name='publisher' control={control} required errors={errors}/>
+            <CustomTextField label='Description' name='description' multiline control={control} required errors={errors}/>
+            <CustomTextField label='Inventory' name='inventory' number notFull minVal={0} control={control} required errors={errors}/>
+            <CustomTextField label='Price' name='price' number notFull startAdornment="$" control={control} required errors={errors}/>
+            <CustomTextField label='Release Date' name='releaseDate' date notFull control={control} required errors={errors}/>
+            <DialogActions>
+                <Button onClick={props.handleCloseDialog} color="error">Cancel</Button>
+                <Button color="success" type="submit">Submit</Button>
+            </DialogActions>
+        </form>
+    );
+}
