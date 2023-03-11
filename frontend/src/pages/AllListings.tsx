6ffbb5a660948955be587dd9bce3fb7cbb670cb8
Jason Gao <jgao2299@gmail.com>
2023-03-10 21:01:44 -0500

    add carousel

--- frontend/src/pages/AllListings.tsx
+++ frontend/src/pages/AllListings.tsx
@@ -1,28 +1,39 @@
 import * as React from "react";
+import { Typography, Divider } from '@mui/material';
 import ListingGrid from "src/components/Listing/ListingGrid";
 import PageHeader from "src/components/PageHeader/PageHeader";
 import { LISTING } from "src/constants/endpoints";
 import { useHttpClient } from "src/hooks/http-hook";
 import { buildListings } from "src/utils/listings";
+import RecommendedCarousel from 'src/components/Recommended/RecommendedCarousel'
 
-export interface IAllListingsProps {}
+export interface IAllListingsProps { }
 
 export default function AllListings(props: IAllListingsProps) {
-  const [listings, setListings] = React.useState([]);
-  const { sendRequest } = useHttpClient();
-  React.useEffect(() => {
-    const getSellerItems = async () => {
-      let items = await sendRequest(LISTING, "GET", {});
-      items = buildListings(items);
-      setListings(items);
-    };
-    getSellerItems();
-  }, [sendRequest]);
-  return (
-    <React.Fragment>
-      <PageHeader headerTitle="Listing">
-        <ListingGrid listings={listings} />
-      </PageHeader>
-    </React.Fragment>
-  );
+    const [listings, setListings] = React.useState([]);
+    const { sendRequest } = useHttpClient();
+    React.useEffect(() => {
+        const getSellerItems = async () => {
+            let items = await sendRequest(LISTING, "GET", {});
+            items = buildListings(items);
+            setListings(items);
+        };
+        getSellerItems();
+    }, [sendRequest]);
+    return (
+        <React.Fragment>
+            <PageHeader headerTitle="Listing">
+                <Typography gutterBottom variant="h3" align='center'>
+                    Recommendations
+                </Typography>
+                <Divider />
+                <RecommendedCarousel listings={listings} />
+                <Typography gutterBottom variant="h3" align='center'>
+                    Listings
+                </Typography>
+                <Divider />
+                <ListingGrid listings={listings} />
+            </PageHeader>
+        </React.Fragment>
+    );
 }
