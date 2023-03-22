d75e4c40d023c664e7bc97108d8f3e92cd26af7c
GolanHassin <golhassin@gmail.com>
2023-03-21 21:10:13 -0400

    Added filtering functionality to ListingGrid.tsx

--- frontend/src/components/Listing/ListingGrid.tsx
+++ frontend/src/components/Listing/ListingGrid.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Grid, Container, Typography, Grow, Paper } from '@mui/material';
+import {Grid, Container, Typography, Grow, Paper, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
 import ListingCard from './ListingCard';
 import 'src/styles/ListingGrid.css'
 
@@ -22,6 +22,7 @@ export interface IListingGridProps {
 }
 
 export default function ListingGrid(props: IListingGridProps) {
+    const [sortingType,setSortingType] = React.useState("A-Z");
     if (props.listings.length === 0) {
         return (
             <Typography variant="h4" align='center' color='text.secondary' mt={2}>
@@ -29,10 +30,53 @@ export default function ListingGrid(props: IListingGridProps) {
             </Typography>
         );
     }
+
+    const sortListings = (listings : Array<any>) => {
+        console.log("I was called");
+        const myClonedArray: any[] = [];
+        listings.forEach(val => myClonedArray.push(Object.assign({}, val)));
+        switch (sortingType){
+            case "A-Z":
+                myClonedArray.sort((a,b)=>(a.title>b.title)? 1: -1);
+                break;
+            case "Z-A":
+                myClonedArray.sort((a,b)=>(b.title>a.title)? 1: -1);
+                break;
+            case "Expensive":
+                myClonedArray.sort((a,b)=>(a.price<b.price)? 1: -1);
+                break;
+            case "Cheapest":
+                myClonedArray.sort((a,b)=>(b.price<a.price)? 1: -1);
+                break;
+            case "AuthorAlphabetic":
+                myClonedArray.sort((a,b)=>(a.author>b.author)? 1: -1);
+                break;
+        }
+        return myClonedArray;
+    }
+
     return (
         <Container maxWidth="xl" className='grid'>
+            <FormControl>
+                <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                    Listing Sorting
+                </InputLabel>
+                <Select
+                    defaultValue={"A-Z"}
+                    inputProps={{
+                        name: 'listingSorting',
+                        id: 'sortListings',
+                    }}
+                >
+                    <MenuItem value={"A-Z"} onClick={() => setSortingType("A-Z")}>Alphabetic (A-Z)</MenuItem>
+                    <MenuItem value={"Z-A"} onClick={() => setSortingType("Z-A")}>Reverse Alphabetic (Z-A)</MenuItem>
+                    <MenuItem value={"Expensive"} onClick={() => setSortingType("Expensive")}>Price (Most expensive)</MenuItem>
+                    <MenuItem value={"Cheapest"} onClick={() => setSortingType("Cheapest")}>Price (Cheapest)</MenuItem>
+                    <MenuItem value={"AuthorAlphabetic"}onClick={() => setSortingType("AuthorAlphabetic")}>Author Alphabetic</MenuItem>
+                </Select>
+            </FormControl>
             <Grid container spacing={{ xs: 1, sm: 4 }} columns={{ xs: 1, sm: 8, md: 12 }}>
-                {props.listings.map((x) => {
+                {sortListings(props.listings).map((x) => {
                     return (
                         <Grid item xs={1} sm={4} md={4} key={uuidv4()}>
                             <Grow in={true} timeout={1000}>
