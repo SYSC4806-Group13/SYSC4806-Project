b6001da282886d8f7056086a6e78f027a34c1db4
GolanHassin <51271214+GolanHassin@users.noreply.github.com>
2023-03-21 22:35:39 -0400

    48: Filtering Sorting for listings (#58)
    * Added filtering functionality to ListingGrid.tsx
    
    * Responding to pull request comments
    
    * Small change to make sure Reverse Alphabetic and Alphabetic
    sorting works.

--- frontend/src/components/Listing/ListingGrid.tsx
+++ frontend/src/components/Listing/ListingGrid.tsx
@@ -1,28 +1,52 @@
 import * as React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Grid, Container, Typography, Grow, Paper } from '@mui/material';
+import {Grid, Container, Typography, Grow, Paper, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
 import ListingCard from 'src/components/Listing/ListingCard';
-import 'src/styles/ListingGrid.css';
+import 'src/styles/ListingGrid.css'
 
 export interface IListingGridProps {
-  listings: Array<{
-    listingId: string,
-    cardName: string,
-    author: string,
-    price: string,
-    image: string,
-    alt: string,
-    title: string,
-    isbn: string,
-    publisher: string,
-    description: string,
-    inventory: string,
-    releaseDate: string
-  }>
+    listings: Array<{
+        cardName: string,
+        author: string,
+        price: string,
+        image: string,
+        alt: string,
+        title: string,
+        listingId : string,
+        isbn: string,
+        publisher: string,
+        description: string,
+        inventory: string,
+        releaseDate: string
+    }>
 }
 
 export default function ListingGrid({ listings }: IListingGridProps) {
-  return (
+    const [sortingType,setSortingType] = React.useState("Alphabetic");
+    const sortListings = (listings : Array<any>) => {
+        const myClonedArray: any[] = [];
+        listings.forEach(val => myClonedArray.push(Object.assign({}, val)));
+        switch (sortingType){
+            case "Alphabetic":
+                myClonedArray.sort((a,b)=>(a.title>b.title)? 1: -1);
+                break;
+            case "ReverseAlphabetic":
+                myClonedArray.sort((a,b)=>(b.title>a.title)? 1: -1);
+                break;
+            case "Expensive":
+                myClonedArray.sort((a,b)=>(a.price<b.price)? 1: -1);
+                break;
+            case "Cheapest":
+                myClonedArray.sort((a,b)=>(b.price<a.price)? 1: -1);
+                break;
+            case "AuthorAlphabetic":
+                myClonedArray.sort((a,b)=>(a.author>b.author)? 1: -1);
+                break;
+        }
+        return myClonedArray;
+    }
+
+    return (
     <>
       {listings.length === 0 &&
         <Typography variant="h4" align='center' color='text.secondary' mt={2}>
@@ -31,8 +55,26 @@ export default function ListingGrid({ listings }: IListingGridProps) {
       }
       {listings.length !== 0 &&
         <Container maxWidth="xl" className='grid'>
+            <FormControl>
+                <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                    Listing Sorting
+                </InputLabel>
+                <Select
+                    defaultValue={"Alphabetic"}
+                    inputProps={{
+                        name: 'listingSorting',
+                        id: 'sortListings',
+                    }}
+                >
+                    <MenuItem value={"Alphabetic"} onClick={() => setSortingType("Alphabetic")}>Alphabetic (A-Z)</MenuItem>
+                    <MenuItem value={"ReverseAlphabetic"} onClick={() => setSortingType("ReverseAlphabetic")}>Reverse Alphabetic (Z-A)</MenuItem>
+                    <MenuItem value={"Expensive"} onClick={() => setSortingType("Expensive")}>Price (Most expensive)</MenuItem>
+                    <MenuItem value={"Cheapest"} onClick={() => setSortingType("Cheapest")}>Price (Cheapest)</MenuItem>
+                    <MenuItem value={"AuthorAlphabetic"}onClick={() => setSortingType("AuthorAlphabetic")}>Author Alphabetic</MenuItem>
+                </Select>
+            </FormControl>
           <Grid container spacing={{ xs: 1, sm: 4 }} columns={{ xs: 1, sm: 8, md: 12 }}>
-            {listings.map((currentListing) => {
+            {sortListings(listings).map((currentListing) => {
               return (
                 <Grid item xs={1} sm={4} md={4} key={uuidv4()}>
                   <Grow in={true} timeout={1000}>
