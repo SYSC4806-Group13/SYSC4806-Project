3cb07ec4420c8681adb4be60870d53af9774dd96
Jason Gao <jgao2299@gmail.com>
2023-03-26 13:39:52 -0400

    test

--- Backend/src/test/java/com/SYSC4806_Group13/SYSC4806_Project/Controllers/RecommendationRestControllerTest.java
+++ Backend/src/test/java/com/SYSC4806_Group13/SYSC4806_Project/Controllers/RecommendationRestControllerTest.java
@@ -35,61 +35,102 @@ public class RecommendationRestControllerTest {
     private MockMvc mockMvc;
     private String token;
 
+    private ObjectMapper mapper;
+    private User user1;
+    private User user2;
+    private Long user1Id;
+    private Long user2Id;
+    private Listing listing1;
+    private Listing listing2;
+
     @BeforeEach
     public void beforeTest() {
         //Add the SuperUser and token
-        usersUtil.setSuperUserInRepo();
-        this.token = usersUtil.getSuperUserToken();
+        this.usersUtil.setSuperUserInRepo();
+        this.token = this.usersUtil.getSuperUserToken();
+
+        this.mapper = new ObjectMapper();
+
+        // create users
+        this.user1 = new User();
+        this.user2 = new User();
+        this.userRepository.save(this.user1);
+        this.userRepository.save(this.user2);
+
+        // get user id
+        this.user1Id = this.user1.getId();
+        this.user2Id = this.user2.getId();
+
+        // create listings
+        this.listing1 = new Listing(this.user1Id, "1", "1", 0.0F, "1", "1", "1", 5, "1", true);
+        this.listing2 = new Listing(this.user1Id, "2", "2", 0.0F, "2", "2", "2", 5, "2", true);
+        this.listingRepository.save(this.listing1);
+        this.listingRepository.save(this.listing2);
+
     }
 
     @AfterEach
     public void resetTest() {
+        // delete users
+        this.userRepository.deleteById(this.user1.getId());
+        this.userRepository.deleteById(this.user2.getId());
         // Reset the database to prevent tests from affecting each other
-        cartItemRepository.deleteAll();
-        listingRepository.deleteAll();
+        this.cartItemRepository.deleteAll();
+        this.listingRepository.deleteAll();
     }
 
     @Test
-    void recommendation_test() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-
-        // create user
-        User user1 = new User();
-        User user2 = new User();
-        userRepository.save(user1);
-        userRepository.save(user2);
-
-        // get user id
-        Long userId = user1.getId();
-        Long userId2 = user2.getId();
-
-        // create listings
-        Listing listing1 = new Listing(userId, "1", "1", 0.0F, "1", "1", "1", 5, "1", true);
-        Listing listing2 = new Listing(userId, "2", "2", 0.0F, "2", "2", "2", 5, "2", true);
-        listingRepository.save(listing1);
-        listingRepository.save(listing2);
-
+    void recommendation_test_one_not_seen() throws Exception {
         // create checked out cart items
-        CartItem cartItem1 = new CartItem(userId, listing1, 1L);
-        CartItem cartItem2 = new CartItem(userId, listing2, 1L);
-        CartItem cartItem3 = new CartItem(userId2, listing1, 1L);
+        CartItem cartItem1 = new CartItem(this.user1Id, this.listing1, 1L);
+        CartItem cartItem2 = new CartItem(this.user1Id, this.listing2, 1L);
+        CartItem cartItem3 = new CartItem(this.user2Id, this.listing1, 1L);
         cartItem1.checkout();
         cartItem2.checkout();
         cartItem3.checkout();
-        cartItemRepository.save(cartItem1);
-        cartItemRepository.save(cartItem2);
-        cartItemRepository.save(cartItem3);
+        this.cartItemRepository.save(cartItem1);
+        this.cartItemRepository.save(cartItem2);
+        this.cartItemRepository.save(cartItem3);
 
         // get recommendations
-        MvcResult result = mockMvc.perform(get("/recommendation/" + userId2)
-                        .header("Authorization", "Bearer " + token))
+        MvcResult result = this.mockMvc.perform(get("/recommendation/" + this.user2Id)
+                        .header("Authorization", "Bearer " + this.token))
                         .andExpect(status().is2xxSuccessful())
                         .andReturn();
-        Set recommendations = mapper.readValue(result.getResponse().getContentAsByteArray(), Set.class);
+        Set recommendations = this.mapper.readValue(result.getResponse().getContentAsByteArray(), Set.class);
         Assert.isTrue(recommendations.size() == 1, "Recommendation list should have size 1");
 
         // delete users
-        userRepository.deleteById(userId);
-        userRepository.deleteById(userId2);
+        this.userRepository.deleteById(this.user1Id);
+        this.userRepository.deleteById(this.user2Id);
+    }
+
+    @Test
+    void recommendation_test_all_seen() throws Exception {
+        // create checked out cart items
+        CartItem cartItem1 = new CartItem(this.user1Id, this.listing1, 1L);
+        CartItem cartItem2 = new CartItem(this.user1Id, this.listing2, 1L);
+        CartItem cartItem3 = new CartItem(this.user2Id, this.listing1, 1L);
+        CartItem cartItem4 = new CartItem(this.user2Id, this.listing2, 1L);
+        cartItem1.checkout();
+        cartItem2.checkout();
+        cartItem3.checkout();
+        cartItem4.checkout();
+        this.cartItemRepository.save(cartItem1);
+        this.cartItemRepository.save(cartItem2);
+        this.cartItemRepository.save(cartItem3);
+        this.cartItemRepository.save(cartItem4);
+
+        // get recommendations
+        MvcResult result = mockMvc.perform(get("/recommendation/" + this.user2Id)
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        Set recommendations = mapper.readValue(result.getResponse().getContentAsByteArray(), Set.class);
+        Assert.isTrue(recommendations.size() == 0, "Recommendation list should have size 0");
+
+        // delete users
+        userRepository.deleteById(this.user1Id);
+        userRepository.deleteById(this.user2Id);
     }
 }
